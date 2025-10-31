096a320564bd383ceeba9a728871f291b12d5699
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2025-10-31 16:19:27 +0100

    Add core module

---
+++ src/core.vhd
@@ -0,0 +1,15 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+
+entity core is
+	port (
+		clk_in: in std_logic
+	);
+end core;
+
+
+architecture rtl of core is
+begin
+end rtl;
