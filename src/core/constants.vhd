991bd633d9116559e5ed9690a944f87bd3536015
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2025-11-16 20:59:34 +0100

    Add constant for default fetch output

---
+++ src/core/constants.vhd
@@ -0,0 +1,9 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+use work.core_types.all;
+
+
+package core_constants is
+	constant DEFAULT_FETCH_OUTPUT: fetch_output_t := ();
+end package core_constants;
--- src/core/fetch.vhd
+++ src/core/fetch.vhd
@@ -3,12 +3,13 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 use work.core_types.all;
+use work.core_constants.all;
 
 
 entity fetch is
 	port (
 		clk: in std_logic;
-		output: out fetch_output_t
+		output: out fetch_output_t := DEFAULT_FETCH_OUTPUT
 	);
 end fetch;
 
