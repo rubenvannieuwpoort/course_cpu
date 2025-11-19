ccfdd72186bba03efe5542478976e8a3f64d2808
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2025-11-19 21:09:29 +0100

    Add placeholder signals

--- src/core/types.vhd
+++ src/core/types.vhd
@@ -4,14 +4,18 @@ use ieee.std_logic_1164.all;
 
 package core_types is
 	type fetch_output_t is record
+		placeholder: std_logic;
 	end record fetch_output_t;
 
 	type decode_output_t is record
+		placeholder: std_logic;
 	end record decode_output_t;
 
 	type execute_output_t is record
+		placeholder: std_logic;
 	end record execute_output_t;
 
 	type memory_output_t is record
+		placeholder: std_logic;
 	end record memory_output_t;
 end package core_types;
