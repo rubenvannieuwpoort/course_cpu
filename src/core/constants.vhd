76100045a283075d8a6693bf501fd251728eebfd
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2025-11-19 21:09:29 +0100

    Add placeholder signals

--- src/core/constants.vhd
+++ src/core/constants.vhd
@@ -5,8 +5,19 @@ use work.core_types.all;
 
 
 package core_constants is
-	constant DEFAULT_FETCH_OUTPUT: fetch_output_t := ();
-	constant DEFAULT_DECODE_OUTPUT: decode_output_t := ();
-	constant DEFAULT_EXECUTE_OUTPUT: execute_output_t := ();
-	constant DEFAULT_MEMORY_OUTPUT: memory_output_t := ();
+	constant DEFAULT_FETCH_OUTPUT: fetch_output_t := (
+		placeholder => '0'
+	);
+
+	constant DEFAULT_DECODE_OUTPUT: decode_output_t := (
+		placeholder => '0'
+	);
+
+	constant DEFAULT_EXECUTE_OUTPUT: execute_output_t := (
+		placeholder => '0'
+	);
+
+	constant DEFAULT_MEMORY_OUTPUT: memory_output_t := (
+		placeholder => '0'
+	);
 end package core_constants;
