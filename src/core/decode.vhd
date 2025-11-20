06d067fceaf59f9cd1e11331d90a848700c93417
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2025-11-20 21:20:24 +0100

    Recognize ADDI instruction

--- src/core/decode.vhd
+++ src/core/decode.vhd
@@ -19,10 +19,22 @@ architecture rtl of decode is
 begin
 
 	process (clk)
+		variable opcode: std_logic_vector(6 downto 0);
+		variable funct3: std_logic_vector(2 downto 0);
+		variable rs1, rs2, rd : std_logic_vector(4 downto 0);
 	begin
 		if rising_edge(clk) then
+			opcode := input.instr(6 downto 0);
+			rs1    := input.instr(19 downto 15);
+			rs2    := input.instr(24 downto 20);
+			funct3 := input.instr(14 downto 12);
+			rd     := input.instr(11 downto 7);
+
 			if input.is_active = '1' then
-				-- TODO: decode instructions
+				if opcode = "0010011" and funct3 = "000" then
+					-- ADDI rd, rs, imm (I-type): sets rd to the sum of rs1 and the sign-extended immediate
+					-- TODO: set control signals
+				end if;
 			else
 				output <= DEFAULT_DECODE_OUTPUT;
 			end if;
