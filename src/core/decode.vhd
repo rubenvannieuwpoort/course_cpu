cc5be9922a3176257e91e700d0244b21c49414a2
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2025-11-20 21:35:51 +0100

    Set decode output for ADDI

--- src/core/decode.vhd
+++ src/core/decode.vhd
@@ -22,6 +22,9 @@ begin
 		variable opcode: std_logic_vector(6 downto 0);
 		variable funct3: std_logic_vector(2 downto 0);
 		variable rs1, rs2, rd : std_logic_vector(4 downto 0);
+
+		variable i_imm: std_logic_vector(11 downto 0);
+		variable i_imm_s: std_logic_vector(31 downto 0);
 	begin
 		if rising_edge(clk) then
 			opcode := input.instr(6 downto 0);
@@ -30,10 +33,16 @@ begin
 			funct3 := input.instr(14 downto 12);
 			rd     := input.instr(11 downto 7);
 
+			i_imm := input.instr(31 downto 20);
+			i_imm_s := std_logic_vector(resize(signed(i_imm), 32));
+
 			if input.is_active = '1' then
 				if opcode = "0010011" and funct3 = "000" then
 					-- ADDI rd, rs, imm (I-type): sets rd to the sum of rs1 and the sign-extended immediate
-					-- TODO: set control signals
+					output.operation <= OP_ADD;
+					output.operand1 <= (others => '0');  -- TODO: load value from register
+					output.operand2 <= i_imm_s;
+					output.destination_reg <= rd;
 				end if;
 			else
 				output <= DEFAULT_DECODE_OUTPUT;
