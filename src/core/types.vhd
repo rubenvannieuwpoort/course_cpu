2591ac193af829e317ee26a1731a2ed1590977d6
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2025-11-22 10:28:37 +0100

    Add field and enum type for operation

--- src/core/types.vhd
+++ src/core/types.vhd
@@ -3,12 +3,15 @@ use ieee.std_logic_1164.all;
 
 
 package core_types is
+	type operation_t is (OP_ADD);
+
 	type fetch_output_t is record
 		is_active: std_logic;
 		instr: std_logic_vector(31 downto 0);
 	end record fetch_output_t;
 
 	type decode_output_t is record
+		operation: operation_t;
 		operand1: std_logic_vector(31 downto 0);
 		operand2: std_logic_vector(31 downto 0);
 		destination_reg: std_logic_vector(4 downto 0);
