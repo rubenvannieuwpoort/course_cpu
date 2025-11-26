846389a1f2819fc4bb7cd9b5861556ea360ee369
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2025-11-26 20:30:06 +0100

    Implement SLT operation

--- src/core/execute.vhd
+++ src/core/execute.vhd
@@ -29,6 +29,12 @@ begin
 			if input.is_active = '1' and input.is_invalid = '0' then
 				if input.operation = OP_ADD then
 					v_output.result := std_logic_vector(unsigned(input.operand1) + unsigned(input.operand2));
+				elsif input.operation = OP_SLT then
+					if signed(input.operand1) < signed(input.operand2) then
+						v_output.result := std_logic_vector(to_unsigned(1, 32));
+					else
+						v_output.result := (others => '0');
+					end if;
 				elsif input.operation = OP_LED then
 					led <= input.operand1(7 downto 0);
 				else
