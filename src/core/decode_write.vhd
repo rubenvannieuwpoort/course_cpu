9481d115ba1798a27cf3b23977528561248415c9
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2025-11-27 20:54:14 +0100

    Recognize ECALL and EBREAK

--- src/core/decode_write.vhd
+++ src/core/decode_write.vhd
@@ -215,6 +215,10 @@ begin
 					end if;
 				elsif funct3 = "000" and opcode = "0001111" then
 					-- FENCE (implemented as NOP)
+				elsif i_imm = "000000000000" and rs1 = "00000" and funct3 = "000" and rd = "00000" and opcode = "1110011" then
+					-- ECALL
+				elsif i_imm = "000000000001" and rs1 = "00000" and funct3 = "000" and rd = "00000" and opcode = "1110011" then
+					-- EBREAK
 				else
 					v_decode_output.is_invalid := '1';
 				end if;
