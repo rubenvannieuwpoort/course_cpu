1e08b5cb77caec21bf0d23480e9330ed53603714
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2025-12-04 20:41:21 +0100

    Add type and field for memory command

--- src/types.vhd
+++ src/types.vhd
@@ -3,8 +3,11 @@ use ieee.std_logic_1164.all;
 
 
 package types is
+	type mem_cmd_t is (MEM_CMD_READ, MEM_CMD_WRITE);
+
 	type mem_req_t is record
 		active: std_logic;
+		cmd: mem_cmd_t;
 		address: std_logic_vector(31 downto 0);
 		value: std_logic_vector(31 downto 0);
 	end record mem_req_t;
