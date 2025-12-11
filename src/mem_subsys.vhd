1894dd657ce4eb39bed8403165e228e76448957d
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2025-12-11 21:38:05 +0100

    Remove generics from block RAM

--- src/mem_subsys.vhd
+++ src/mem_subsys.vhd
@@ -19,23 +19,17 @@ end mem_subsys;
 
 architecture rtl of mem_subsys is
 	component bram is
-        generic(
-            SIZE: integer := 1024;
-            ADDR_WIDTH: integer := 10;
-            COL_WIDTH: integer := 8;
-            NB_COL: integer := 4
-        );
 		port(
 			clka: in std_logic;
 			ena: in std_logic;
-			wea: in std_logic_vector(NB_COL - 1 downto 0);
-			addra: in std_logic_vector(ADDR_WIDTH - 1 downto 0);
-			dia: in std_logic_vector(NB_COL * COL_WIDTH - 1 downto 0);
-			doa: out std_logic_vector(NB_COL * COL_WIDTH - 1 downto 0);
+			wea: in std_logic_vector(3 downto 0);
+			addra: in std_logic_vector(9 downto 0);
+			dia: in std_logic_vector(31 downto 0);
+			doa: out std_logic_vector(31 downto 0);
 			clkb: in std_logic;
 			enb: in std_logic;
-			addrb: in std_logic_vector(ADDR_WIDTH - 1 downto 0);
-			dob: out std_logic_vector(NB_COL * COL_WIDTH - 1 downto 0)
+			addrb: in std_logic_vector(9 downto 0);
+			dob: out std_logic_vector(31 downto 0)
 		);
 	end component;
 
