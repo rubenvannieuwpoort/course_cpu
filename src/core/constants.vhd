f434984b27078594227c9be98aa39fcda47246d7
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2025-12-13 22:17:23 +0100

    Add constants for mandatory CSRs

--- src/core/constants.vhd
+++ src/core/constants.vhd
@@ -30,4 +30,41 @@ package core_constants is
 		mem_size => SIZE_WORD,
 		mem_addr => "00"
 	);
+
+	-- privileged CSRs
+	constant CSR_MVENDORID: std_logic_vector(11 downto 0) := X"F11";
+	constant CSR_MARCHID: std_logic_vector(11 downto 0) := X"F12";
+	constant CSR_MIMPID: std_logic_vector(11 downto 0) := X"F13";
+	constant CSR_MHARTID: std_logic_vector(11 downto 0) := X"F14";
+	constant CSR_MCONFIGPTR: std_logic_vector(11 downto 0) := X"F15";
+
+	constant CSR_MSTATUS: std_logic_vector(11 downto 0) := X"300";
+	constant CSR_MISA: std_logic_vector(11 downto 0) := X"301";
+	constant CSR_MIE: std_logic_vector(11 downto 0) := X"304";
+	constant CSR_MTVEC: std_logic_vector(11 downto 0) := X"305";
+	constant CSR_MSTATUSH: std_logic_vector(11 downto 0) := X"310";
+
+	constant CSR_MSCRATCH: std_logic_vector(11 downto 0) := X"340";
+	constant CSR_MEPC: std_logic_vector(11 downto 0) := X"341";
+	constant CSR_MCAUSE: std_logic_vector(11 downto 0) := X"342";
+	constant CSR_MTVAL: std_logic_vector(11 downto 0) := X"343";
+	constant CSR_MIP: std_logic_vector(11 downto 0) := X"344";
+
+	constant CSR_MCYCLE: std_logic_vector(11 downto 0) := X"B00";
+	constant CSR_MINSTRET: std_logic_vector(11 downto 0) := X"B02";
+	constant CSR_MHPMCOUNTER3: std_logic_vector(11 downto 0) := X"B03";
+	-- ...
+	constant CSR_MHPMCOUNTER31: std_logic_vector(11 downto 0) := X"B1F";
+	constant CSR_MCYCLEH: std_logic_vector(11 downto 0) := X"B80";
+	constant CSR_MINSTRETH: std_logic_vector(11 downto 0) := X"B82";
+	constant CSR_MHPMCOUNTER3H: std_logic_vector(11 downto 0) := X"B83";
+	-- ...
+	constant CSR_MHPMCOUNTER31H: std_logic_vector(11 downto 0) := X"B9F";
+
+	constant CSR_MHPMEVENT3: std_logic_vector(11 downto 0) := X"323";
+	-- ...
+	constant CSR_MHPMEVENT31: std_logic_vector(11 downto 0) := X"33f";
+	constant CSR_MHPMEVENT3H: std_logic_vector(11 downto 0) := X"723";
+	-- ...
+	constant CSR_MHPMEVENT31H: std_logic_vector(11 downto 0) := X"73f";
 end package core_constants;
