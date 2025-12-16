276b95343d89b35ff375baf72b2422e8ae0b3ede
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2025-12-16 21:05:49 +0100

    Implement mhpmcounter

--- src/core/execute.vhd
+++ src/core/execute.vhd
@@ -260,6 +260,8 @@ begin
 					elsif input.operand2(11 downto 0) = CSR_MINSTRET then
 						v_output.result := minstret;
 						minstret <= (minstret or csr_set_bits) and csr_clear_bits;
+					elsif unsigned(CSR_MHPMCOUNTER3) <= unsigned(input.operand2(11 downto 0)) and unsigned(input.operand2(11 downto 0)) <= unsigned(CSR_MHPMCOUNTER31) then
+						v_output.result := (others => '0');
 					elsif input.csr_read_only = '1' then
 						-- read-only CSRs
 						if input.operand2(11 downto 0) = CSR_MVENDORID then
